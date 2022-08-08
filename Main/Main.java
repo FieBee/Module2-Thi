424fb832eaed821c4c0a16bd843c6c8c948e3ab8
FieBee <phuonglf98@gmail.com>
2022-08-08 11:35:27 +0700

    Hoan thanh

--- Main/Main.java
+++ Main/Main.java
@@ -9,184 +9,126 @@ import java.util.*;
 
 public class Main {
 
-    ProductManager productManager = new ProductManager();
-    private List <Product> productList= productManager.productList;
+//    Main main = new Main();
+static ProductManager productManager = new ProductManager();
     private ReadWriteFile readWriteFile = ReadWriteFile.getInstance();
 
 
     public static void main(String[] args) {
-
         menuManager();
     }
 
 
-    public static void menuManager(){
-        do {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println(" ---- CHƯƠNG TRÌNH QUẢN LÍ SẢN PHẨM ----");
-            System.out.println("Chọn chức năng theo số");
-            System.out.println("1. Xem danh sách");
-            System.out.println("2. Thêm mới");
-            System.out.println("3. Cập nhật ");
-            System.out.println("4. Xóa");
-            System.out.println("5. Sắp xếp");
-            System.out.println("6.Tìm sản phẩm giá đắt nhất");
-            System.out.println("7. Đọc từ file");
-            System.out.println("8. Ghi vào file");
-            System.out.println("9. Thoát");
-            System.out.println("Chọn chức năng: ");
+    public static void menuManager() {
+        try {
+            do {
+                Scanner scanner = new Scanner(System.in);
+                Scanner scanner1 = new Scanner(System.in);
+                System.out.println(" ---- CHƯƠNG TRÌNH QUẢN LÍ SẢN PHẨM ----");
+                System.out.println("Chọn chức năng theo số");
+                System.out.println("1. Xem danh sách");
+                System.out.println("2. Thêm mới");
+                System.out.println("3. Cập nhật ");
+                System.out.println("4. Xóa");
+                System.out.println("5. Sắp xếp");
+                System.out.println("6.Tìm sản phẩm giá đắt nhất");
+                System.out.println("7. Đọc từ file");
+                System.out.println("8. Ghi vào file");
+                System.out.println("9. Thoát");
+                System.out.println("Chọn chức năng: ");
 
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        productManager.displayProduct();
+                        break;
+                    case 2:
+                        System.out.println("Thêm sản phẩm");
+                        System.out.println("Nhập id sản phẩm muốn thêm mới: ");
+                        int id = scanner.nextInt();
+                        System.out.println("Nhập tên sản phẩm muốn tạo mới: ");
+                        String name = scanner1.nextLine();
+                        System.out.println("Nhập giá tiền:");
+                        int price = scanner.nextInt();
+                        System.out.println("Nhập số lượng:");
+                        int quantity = scanner.nextInt();
+                        System.out.println("Nhập mô tả: ");
+                        String describe = scanner1.nextLine();
+                        Product newProduct = new Product(id, name, price, quantity, describe);
+                        productManager.addProduc(newProduct);
 
-                    displayProduct();
-                    break;
-                case 2:
-                    addProduc();
-                    break;
-                case 3:
-                    editProduc();
-                    break;
+                        System.out.println("Thêm sản phẩm thành công!!" + newProduct);
 
-                case 4:
-                    deleteProduct();
-                    break;
-                case 5:
-                    sortProduct();
-                    break;
-                case 6:
-                    findMaxPrice();
-                    break;
-                case 7:
-                    readFile();
-                    break;
-                case 8:
-                    writeFile();
-                    break;
-                case 9:
-                    System.out.println("Bye");
-                    return;
-                default:
-                    break;
-            }
-        } while (true);
-    }
+                        break;
+                    case 3:
+                        try{
+                            System.out.println("Nhập id sản phẩm muốn sửa: ");
+                            int checkId = 0;
+                            do {
+                                checkId = productManager.checkId(scanner.nextInt());
+
+                            }while (checkId <0 && checkId >= productManager.productList.size());
+
+                            if (checkId != -1) {
+                                System.out.println("Nhập tên sản phẩm muốn sửa: ");
+                                String name1 = scanner.nextLine();
+                                System.out.println("Nhập giá tiền:");
+                                int price1 = scanner.nextInt();
+                                System.out.println("Nhập số lượng:");
+                                int quantity1 = scanner.nextInt();
+                                System.out.println("Nhập mô tả: ");
+                                String describe1 = scanner1.nextLine();
+
+                                Product newProduct1 = new Product(checkId, name1, price1, quantity1, describe1);
+
+                                productManager.editProduc(checkId, newProduct1);
+
+                                System.out.println("Sửa sản phẩm thành công!!");
+                            } else {
+                                menuManager();
+                            }
+                            break;
+                        }catch (InputMismatchException e){
+                            System.out.println("Id khong ton tai!!");
+                            menuManager();
+                        }
 
 
-    public static void displayProduct(){
-        int stt = 1;
-        for (Product element: productList
-        ) {
-            System.out.println(stt + ". "+element);
+
+                    case 4:
+                        System.out.println("Nhập id sản phẩm muốn xóa: ");
+                        int id2 = scanner1.nextInt();
+
+                        if (productManager.productList.get(id2) == null) {
+                            System.out.println("Id không tồn tại!!");
+                            menuManager();
+                        } else {
+                            productManager.deleteProduc(id2);
+                        }
+                        break;
+                    case 5:
+                        productManager.sortProduct();
+                        break;
+                    case 6:
+                        productManager.findMaxPrice();
+                        break;
+                    case 7:
+                        productManager.readFile();
+                        break;
+                    case 8:
+                        productManager.writeFile();
+                        break;
+                    case 9:
+                        System.out.println("Bye");
+                        return;
+                    default:
+                        break;
+                }
+            } while (true);
+        }catch (Exception e){
+            System.out.println("Loi");
+            menuManager();
         }
     }
-
-    public static void addProduc(){
-        Scanner scanner  = new Scanner(System.in);
-        Scanner scanner1  = new Scanner(System.in);
-        System.out.println("Thêm sản phẩm");
-        System.out.println("Nhập id sản phẩm muốn thêm mới: ");
-        int id = scanner1.nextInt();
-        System.out.println("Nhập tên sản phẩm muốn tạo mới: ");
-        String name = scanner.nextLine();
-        System.out.println("Nhập giá tiền:");
-        int price = scanner.nextInt();
-        System.out.println("Nhập số lượng:");
-        int quantity = scanner.nextInt();
-        System.out.println("Nhập mô tả: ");
-        String describe = scanner1.nextLine();
-        Product newProduct = new Product(id,name,price,quantity,describe);
-        productList.add(newProduct);
-
-        System.out.println("Thêm sản phẩm thành công : "+ newProduct);
-    }
-
-    public static void editProduc(){
-        Scanner scanner  = new Scanner(System.in);
-        System.out.println("Sửa sản phẩm");
-
-        int checkID = checkID();
-
-        if (productList.get(checkID) == null){
-            System.out.println("Id không tồn tại!!");
-            return;
-        }else {
-            System.out.println("Nhập tên mới: ");
-            productList.get(checkID).setName(scanner.nextLine())  ;
-            System.out.println("Nhập giá tiền:");
-            productList.get(checkID).setPrice(scanner.nextInt()) ;
-            System.out.println("Nhập số lượng:");
-            productList.get(checkID).setQuantity(scanner.nextInt() );
-            System.out.println("Nhập mô tả: ");
-            productList.get(checkID).setDescribe( scanner.nextLine() );
-        }
-    }
-    public static int checkID(){
-        Scanner scanner  = new Scanner(System.in);
-        System.out.println("Nhập id sản phẩm can check: ");
-        int checkID = scanner.nextInt();
-
-        if (productList.get(checkID) == null){
-            System.out.println("Id không tồn tại!!");
-            return -1;
-        }
-        else {
-            return checkID;
-        }
-    }
-
-    public static void deleteProduct(){
-        int checkId = checkID();
-        productList.remove(checkId);
-    }
-
-    public static void sortProduct(){
-        Collections.sort(productList, new Comparator<Product>() {
-            public int compare(Product o1, Product o2) {
-                return o1.getPrice() - o2.getPrice();
-            }
-        });
-        displayProduct();
-    }
-
-    public static void findMaxPrice(){
-
-
-        Collections.sort(productList, new Comparator<Product>() {
-            public int compare(Product o1, Product o2) {
-                return o1.getPrice() - o2.getPrice();
-            }
-        });
-        System.out.println(" Sản phẩm giá đắt nhất là: ");
-        System.out.println(productList.get(productList.size()-1));
-
-    }
-
-    public static void writeFile(){
-        try{
-            readWriteFile.writeData(productList);
-            System.out.println("");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public static void readFile(){
-        try{
-            productList = readWriteFile.readData();
-            displayProduct();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-
-
-
-
-
 
 }
--- controller/ProductManager.java
+++ controller/ProductManager.java
@@ -2,12 +2,17 @@ package controller;
 
 import jdk.jfr.internal.tool.Main;
 import model.Product;
+import storage.ReadWriteFile;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ProductManager {
-    public List<Product> productList = new ArrayList<>();
+
+    private ReadWriteFile readWriteFile = ReadWriteFile.getInstance();
+    public List<Product> productList = readWriteFile.readData();
 
 
     public  void displayProduct(){
@@ -22,7 +27,59 @@ public class ProductManager {
         productList.add(product);
     }
 
-    public void editProduc(int id){
+    public void editProduc(int id, Product product){
+        productList.set(id, product);
+    }
+
+    public void deleteProduc(int id){
+        productList.remove(id);
+    }
+
+    public void sortProduct(){
+        Collections.sort(productList, new Comparator<Product>() {
+            public int compare(Product o1, Product o2) {
+                return o1.getPrice() - o2.getPrice();
+            }
+        });
+        displayProduct();
+    }
+    public void writeFile(){
+        try{
+            readWriteFile.writeData(productList);
+            System.out.println("");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
+    public void readFile(){
+        try{
+            productList = readWriteFile.readData();
+            displayProduct();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public  void findMaxPrice(){
+        productList.sort(new Comparator<Product>() {
+            public int compare(Product o1, Product o2) {
+                return o1.getPrice() - o2.getPrice();
+            }
+        });
+        System.out.println(" Sản phẩm giá đắt nhất là: ");
+        System.out.println(productList.get(productList.size()-1));
+
+    }
+
+    public int checkId(int id){
+        if (productList.get(id) != null){
+            return id;
+        }else {
+            System.out.println();
+        }
+        return -1;
+    }
+
 }
